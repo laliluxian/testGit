1c42aa1e0e57627ca6f3d405fd85150b6821c06a
Administrator <laliluxian@126.com>
2019-11-20 14:05:14 +0800

    第一次提交 测试123

---
+++ src/main/java/Demo1.java
@@ -0,0 +1,10 @@
+package main.java;
+
+/**
+ * Created by Administrator on 2019/11/20 0020.
+ */
+public class Demo1 {
+    public static void main(String[]args){
+        System.out.println("123");
+    }
+}
